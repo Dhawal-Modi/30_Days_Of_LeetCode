404ff78b4ac62cac75f49827ce616b7333003772
Dhawal <utsavmodi10@gmail.com>
2020-04-10 23:28:24 +0530

    Solution to Problem 9

---
+++ backspaceCompare.py
@@ -0,0 +1,15 @@
+def backspaceCompare(self, S: str, T: str) -> bool:
+    def build(S):
+        ans = []
+        for x in S:
+            if x != '#':
+                ans.append(x)
+            elif ans:
+                ans.pop()
+        return "".join(ans)
+    return build(S) == build(T)
+    
+if __name__ == "__main__":
+    S = str(input(),"\n")
+    T = str(input(),"\n")
+    backspaceCompare(S,T)
