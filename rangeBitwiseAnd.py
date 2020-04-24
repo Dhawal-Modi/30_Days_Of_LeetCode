a525513b23b25a53f76a236af6f17d74b9b1c280
Dhawal <utsavmodi10@gmail.com>
2020-04-24 13:11:01 +0530

    Solution to Problem 23

---
+++ rangeBitwiseAnd.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rangeBitwiseAnd(self, m: int, n: int) -> int:
+        k = 0
+        while True:
+            if n > m:
+                k += 1
+            else:
+                return m << k
+            m = m >> 1
+            n = n >> 1
+                    
