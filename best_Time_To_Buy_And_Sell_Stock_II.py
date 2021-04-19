472d36dec04576e1ecd4aa7f7609464c99e2f6ba
Dhawal <utsavmodi10@gmail.com>
2021-04-19 19:15:09 +0530

    Provided Input Stub

--- best_Time_To_Buy_And_Sell_Stock_II.py
+++ best_Time_To_Buy_And_Sell_Stock_II.py
@@ -1,7 +1,11 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        diff = 0
-        for i in range(1,len(prices)):
-            if prices[i]>prices[i-1]:
-                diff += prices[i]-prices[i-1]
-        return diff
+def maxProfit(prices):
+    diff = 0
+    for i in range(1, len(prices)):
+        if prices[i] > prices[i-1]:
+            diff += prices[i]-prices[i-1]
+    return diff
+
+
+if __name__ == "__main__":
+    n = list(map(int, input().split()))
+    maxProfit(n)
